2d1635e6924cea22088fd1b12abde5dadde9c517
Anericokakai <challykiddoh@gmail.com>
2023-06-18 22:47:40 +0300

    react home

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import About from './components/About'
+import Home from './components/Home'
+
+const App=()=>{
+    return(
+        <div>
+            <Home/>
+            <Home/>
+            <About></About>
+           
+        </div>
+    )
+
+}
+export default App
